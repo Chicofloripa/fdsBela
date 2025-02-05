d1aad3f84f02587750967a11f146be888fd49031
costafap <costafapc@saude.sc.gov.br>
2025-02-05 12:10:07 -0300

    Updates

---
+++ precache-manifest.f5ab62b286496e90d6274e3393ccac20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046013a5c7a8bcc631a2a2f3bc24ae3b",
+    "url": "/fdsBela/index.html"
+  },
+  {
+    "revision": "f5acfe6a084bdb848534",
+    "url": "/fdsBela/static/css/main.c2846a66.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/fdsBela/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "f457a91e29f5e6cff1f0",
+    "url": "/fdsBela/static/js/3.6757c1ca.chunk.js"
+  },
+  {
+    "revision": "f5acfe6a084bdb848534",
+    "url": "/fdsBela/static/js/main.381cfc64.chunk.js"
+  },
+  {
+    "revision": "639f686ad81fcf795931",
+    "url": "/fdsBela/static/js/runtime~main.01022b8b.js"
+  }
+]);
